4aadf50360eaa804278749c2f16566b3a9877cf1
a-harms <hharmsa@gmail.com>
2025-08-22 01:31:21 -0500

    updated calculate_run_statistics.sh to extract more data fields/statistics

--- statistics_calculation/calculate_run_statistics.sh
+++ statistics_calculation/calculate_run_statistics.sh
@@ -13,7 +13,8 @@ plots_outdir=./plots_outdir/
 mkdir -p ./stats_output_files
 time=$(date '+%F_%H-%M-%S')
 stats_output_file='./stats_output_files/stats_output_file_'${time}'.csv'
-input_file="pos_and_neg_sf.csv"
+#input_file="pos_and_neg_sf.csv"
+input_file="new.csv"
 
 
 
@@ -25,7 +26,7 @@ start_line=$1
 end_line=$2
 
 # write column headings to stats output file
-echo 'VME run number,Number of spills,Muon entries,Pion entries,Average momentum' > $stats_output_file
+echo 'VME run number,Number of spills,Number of events,Muon entries,Pion entries,Electron entries,Average momentum,Muon momentum,Pion momentum,Muons/spill,Pions/spill,Events/spill,Muon mean time,Pion mean time',Electron mean time > $stats_output_file
 
 sed -n "${start_line},${end_line}p" $input_file | grep -v '^#' | while IFS= read -r line; do
 	vme_run_num=$(awk -F, '{print $1}' <<< $line)
@@ -35,16 +36,28 @@ sed -n "${start_line},${end_line}p" $input_file | grep -v '^#' | while IFS= read
 
 	# run wcte_vme_plots_new_arguments.py (make the plots) and keep track of the number of spills
 	echo "Running python wcte_vme_plots_new_arguments_2.py for vme run number:" $vme_run_num
-	num_spills=$(python wcte_vme_plots_new_arguments.py $vme_run_num $upper_pion_cut $lower_muon_cut $upper_muon_cut $calib_dir $plots_outdir \
-		| grep 'number of spills' | sed 's/\[INFO\] Total number of spills://')
+
+	plots_new_output=$(python wcte_vme_plots_new_arguments.py $vme_run_num $upper_pion_cut $lower_muon_cut $upper_muon_cut $calib_dir $plots_outdir)
+	echo "${plots_new_output}"
+
+	num_spills=$( grep 'Total number of spills' <<< $plots_new_output| sed 's/\[INFO\] Total number of spills: //')
+	num_events=$( grep 'Total events' <<< $plots_new_output| sed 's/\[INFO\] Total events: //')
 
 	# run wcte_vme_momentum_new_arguments.py
 	echo "Running python wcte_vme_momentum_new_arguments.py for vme run number:" $vme_run_num
 	momentum_new_output=$(python wcte_vme_momentum_new_arguments.py $vme_run_num $plots_outdir)
 
-	# calculate the muon and pion statistics
+	echo "${momentum_new_output}"
+
+	# extracting the muon and pion statistics
 	muon_entries=$(awk '/Muon entries/ {print $7}' <<< $momentum_new_output)
 	pion_entries=$(awk '/Pion entries/ {print $7}' <<< $momentum_new_output)
+	electron_entries=$(awk '/Electron entries/ {print $7}' <<< $momentum_new_output)
+
+	# extracting time of flights
+	muon_mean_time=$(awk '/Muon mean time/ {print $4}' <<< $momentum_new_output)
+	pion_mean_time=$(awk '/Pion mean time/ {print $4}' <<< $momentum_new_output)
+	electron_mean_time=$(awk '/Electron mean time/ {print $4}' <<< $momentum_new_output)
 
 	# calculating the average momentum
 	muon_momentum=$(awk '/p=/ {print $15}' <<< $momentum_new_output | tr -d p= | awk 'NR==1')
@@ -52,8 +65,13 @@ sed -n "${start_line},${end_line}p" $input_file | grep -v '^#' | while IFS= read
 	combined_momentum=$(echo "scale=2 ; $muon_momentum + $pion_momentum" | bc)
 	avg_momentum=$(echo "scale=2 ; $combined_momentum / 2" | bc)
 
-	#writing run information to text file
-	echo "${vme_run_num},${num_spills},${muon_entries},${pion_entries},${avg_momentum}" >> $stats_output_file
+	# calculating per spill statistics
+	muons_per_spill=$(echo "scale=2 ; $muon_entries / $num_spills" | bc)
+	pions_per_spill=$(echo "scale=2 ; $pion_entries / $num_spills" | bc)
+	events_per_spill=$(echo "scale=2 ; $num_events / $num_spills" | bc)
+
+	#writing run information to stats output file
+	echo "${vme_run_num},${num_spills},${num_events},${muon_entries},${pion_entries},${electron_entries},${avg_momentum},${muon_momentum},${pion_momentum},${muons_per_spill},${pions_per_spill},${events_per_spill},${muon_mean_time},${pion_mean_time},${electron_mean_time}" >> $stats_output_file
 
 done
 
